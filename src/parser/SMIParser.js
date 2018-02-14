2608a9ac6d7b44ffce05ac27734b3c8377dd3ce1
u4bi <admin@naver.id>
2018-02-15 08:28:37 +0900

    Updated SMI parser.

--- src/parser/SMIParser.js
+++ src/parser/SMIParser.js
@@ -1,16 +1,19 @@
 export var SMIParser = (subtitle) => {
     let data = [];
 
-    let cue = subtitle.match(/(<SYNC[^]*?)(?=\s*<SYNC|\s*<\/BODY)/gi)
-                      .map(e => e.match(/<SYNC Start=(\d+)><P.*>(.*)?/));
+    let cue = subtitle.replace(/(\r\n|\n|\r)/gm, '')
+                        .match(/(<SYNC[^]*?)(?=\s*<SYNC|\s*<\/BODY)/gi)
+                        .map(e => [ +e.split('><')[0].replace('<SYNC Start=', ''), e.split('><')[1]
+                        .replace(/<br>/g, '\n')
+                        .split('>')[1]]);
 
     cue.forEach( (e, i) => {
 
         data.push({
             id : i + 1,
-            start : (e[1]) / 1000,
-            end   : (cue[i+1] ? cue[i+1][1] : e[1]) / 1000,
-            text  : e[2]
+            start : (e[0]) / 1000,
+            end   : (cue[i+1] ? cue[i+1][0] : e[0]) / 1000,
+            text  : e[1]
         });
 
     });
