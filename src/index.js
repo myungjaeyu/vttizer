e4c9a5d4cd3cd70b34c54ac03ec11a3210e900b9
u4bi <admin@naver.id>
2018-02-15 02:18:44 +0900

    Added vttizer structure.

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { VTTParser } from './parser/index';
+
+class Vttizer {
+    
+    constructor(subtitle, format) {
+
+        this.subtitle = subtitle;
+        this.format = format || 'vtt';
+        this.data = this._process(this.format, this.subtitle);
+
+    }
+
+    _process(format, subtitle) {
+
+        switch(format) {
+            case 'vtt' : return [];
+            case 'srt' : return [];
+            case 'smi' : return [];
+            default : return [];
+        }
+
+    }
+
+}
+
+export default Vttizer;
